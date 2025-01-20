ca1ebbbc15d7d05c20f9f8c6a074fb2e27a91598
thomas <thomas.graber310@gmail.com>
2025-01-20 15:54:01 +0100

    feat(dto/validation): add validators for email and password input

---
+++ src/main/java/graber/thomas/FeastVerse/dto/validation/PasswordValidator.java
@@ -0,0 +1,13 @@
+package graber.thomas.FeastVerse.dto.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        String regex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+        return password.matches(regex);
+    }
+}
