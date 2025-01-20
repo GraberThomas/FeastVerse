eb4ec08981f4bb99965eaa10585117c770bb8e6d
thomas <thomas.graber310@gmail.com>
2025-01-20 15:55:52 +0100

    feat(dto): use @Valid functionnality to valid data

--- src/main/java/graber/thomas/FeastVerse/dto/AuthenticationRequestCreateDto.java
+++ src/main/java/graber/thomas/FeastVerse/dto/AuthenticationRequestCreateDto.java
@@ -1,7 +1,26 @@
 package graber.thomas.FeastVerse.dto;
 
+import graber.thomas.FeastVerse.dto.validation.ValidEmail;
+import graber.thomas.FeastVerse.dto.validation.ValidPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 /**
  * Authentication request DTO record
  */
-public record AuthenticationRequestCreateDto(String firstName, String lastName, String email, String password) {
+public record AuthenticationRequestCreateDto(
+        @NotBlank(message = "Firstname can't be empty.")
+        String firstName,
+
+        @NotBlank(message = "Lastname can't be empty.")
+        String lastName,
+
+        @NotBlank(message = "Email can't be empty.")
+        @ValidEmail
+        String email,
+
+        @NotBlank(message = "Password can't be empty.")
+        @ValidPassword
+        String password
+) {
 }
--- src/main/java/graber/thomas/FeastVerse/dto/AuthenticationRequestDto.java
+++ src/main/java/graber/thomas/FeastVerse/dto/AuthenticationRequestDto.java
@@ -1,7 +1,15 @@
 package graber.thomas.FeastVerse.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 /**
  * Authentication request DTO record
  */
-public record AuthenticationRequestDto(String email, String password) {
+public record AuthenticationRequestDto(
+        @NotBlank(message = "Email can't be empty.")
+        String email,
+
+        @NotBlank(message = "Password can't be empty.")
+        String password
+) {
 }
