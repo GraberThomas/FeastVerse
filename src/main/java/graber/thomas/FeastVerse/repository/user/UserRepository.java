17b1e9dc68fb78ea7d0d29e7b12a28659572071a
thomas <thomas.graber310@gmail.com>
2025-01-21 11:54:02 +0100

    refactor(repository): remove unused elements

--- src/main/java/graber/thomas/FeastVerse/repository/user/UserRepository.java
+++ src/main/java/graber/thomas/FeastVerse/repository/user/UserRepository.java
@@ -1,18 +1,11 @@
 package graber.thomas.FeastVerse.repository.user;
 
 import graber.thomas.FeastVerse.model.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.Set;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
     User findUserByEmail(String email);
-    User findUserByFirstName(String firstName);
     User findUserByPseudo(String pseudo);
 }
