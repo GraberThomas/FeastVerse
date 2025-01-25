9ad0b07c05c92a43f77b713d343e4da7b4cfcbd0
thomas <thomas.graber310@gmail.com>
2025-01-25 14:26:41 +0100

    feat(repository): add repository for ingredientType

---
+++ src/main/java/graber/thomas/feastverse/repository/ingredients/IngredientTypeRepository.java
@@ -0,0 +1,8 @@
+package graber.thomas.feastverse.repository.ingredients;
+
+import graber.thomas.feastverse.model.ingredient.IngredientType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface IngredientTypeRepository extends JpaRepository<IngredientType, Long> {
+}
