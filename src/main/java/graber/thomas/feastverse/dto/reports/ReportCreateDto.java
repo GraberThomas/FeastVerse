6ebea34f0c1635b556a0362687a751026f336208
thomas <thomas.graber310@gmail.com>
2025-01-25 11:59:47 +0100

    feat(dto/report): change type for uid

--- src/main/java/graber/thomas/feastverse/dto/reports/ReportCreateDto.java
+++ src/main/java/graber/thomas/feastverse/dto/reports/ReportCreateDto.java
@@ -4,11 +4,15 @@ import graber.thomas.feastverse.dto.validation.ValidUUID;
 import graber.thomas.feastverse.model.report.ReportType;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
 
 public record ReportCreateDto(
         @NotEmpty
-        @ValidUUID
-        String targetId,
+        UUID targetId,
+
+        @Length(max = 1000)
         String message,
 
         @NotNull
