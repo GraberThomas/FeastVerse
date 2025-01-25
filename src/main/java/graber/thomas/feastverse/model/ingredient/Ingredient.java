071411d47f16158e1566d446066cb2052cc71be5
thomas <thomas.graber310@gmail.com>
2025-01-25 14:25:26 +0100

    feat(model/ingredients): add model for ingredient and ingredientType

---
+++ src/main/java/graber/thomas/feastverse/model/ingredient/Ingredient.java
@@ -0,0 +1,56 @@
+package graber.thomas.feastverse.model.ingredient;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "ingredients")
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "type_id", nullable = false) // La colonne dans la table "ingredients"
+    private IngredientType type;
+
+    private String image_url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public IngredientType getType() {
+        return type;
+    }
+
+    public void setType(IngredientType type) {
+        this.type = type;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+}
---
+++ src/main/java/graber/thomas/feastverse/model/ingredient/IngredientType.java
@@ -0,0 +1,75 @@
+package graber.thomas.feastverse.model.ingredient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import jakarta.persistence.*;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ingredients_type")
+public class IngredientType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Length(max = 1000)
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "type", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    private String image_file_name;
+
+    @Transient
+    public String getImage_url() {
+        return "/images/ingredient_type/" + image_file_name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getImage_file_name() {
+        return image_file_name;
+    }
+
+    public void setImage_file_name(String image_name) {
+        this.image_file_name = image_name;
+    }
+}
