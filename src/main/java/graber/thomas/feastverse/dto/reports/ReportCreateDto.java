de69c6dbf3e00af1c99b27cf86973b149e564cf2
thomas <thomas.graber310@gmail.com>
2025-01-25 12:50:08 +0100

    fix(dto/report): UUID cannot be null

--- src/main/java/graber/thomas/feastverse/dto/reports/ReportCreateDto.java
+++ src/main/java/graber/thomas/feastverse/dto/reports/ReportCreateDto.java
@@ -1,15 +1,13 @@
 package graber.thomas.feastverse.dto.reports;
 
-import graber.thomas.feastverse.dto.validation.ValidUUID;
 import graber.thomas.feastverse.model.report.ReportType;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
 public record ReportCreateDto(
-        @NotEmpty
+        @NotNull
         UUID targetId,
 
         @Length(max = 1000)
