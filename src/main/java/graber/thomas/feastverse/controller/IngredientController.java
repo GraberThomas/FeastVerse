8d1e8339ecdbbbec7651a5cd0c1412a3192538b6
thomas <thomas.graber310@gmail.com>
2025-01-25 19:12:59 +0100

    feat(controller/ingredients): add getall

--- src/main/java/graber/thomas/feastverse/controller/IngredientController.java
+++ src/main/java/graber/thomas/feastverse/controller/IngredientController.java
@@ -1,15 +1,13 @@
 package graber.thomas.feastverse.controller;
 
 import graber.thomas.feastverse.dto.ingredient.IngredientTypeViewDto;
+import graber.thomas.feastverse.dto.ingredient.IngredientViewDto;
 import graber.thomas.feastverse.service.ingredient.IngredientService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @Tag(name = "Ingredient", description = "Endpoints for ingredients")
@@ -32,10 +30,18 @@ public class IngredientController {
     }
 
     @GetMapping("/types/{typeId}")
-    public IngredientTypeViewDto getById(@org.springframework.web.bind.annotation.PathVariable Long typeId) {
+    public IngredientTypeViewDto getById(@PathVariable Long typeId) {
         return IngredientTypeViewDto.fromEntity(ingredientService.getById(typeId).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Ingredient type not found for id " + typeId + ".")
         ));
     }
 
+    @GetMapping
+    public Page<IngredientViewDto> getAllIngredients(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Long type,
+            Pageable pageable
+    ) {
+        return ingredientService.getAllIngredients(name, type, pageable).map(IngredientViewDto::fromEntity);
+    }
 }
--- src/main/java/graber/thomas/feastverse/repository/ingredients/IngredientRepository.java
+++ src/main/java/graber/thomas/feastverse/repository/ingredients/IngredientRepository.java
@@ -2,7 +2,8 @@ package graber.thomas.feastverse.repository.ingredients;
 
 import graber.thomas.feastverse.model.ingredient.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 
-public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+public interface IngredientRepository extends JpaRepository<Ingredient, Long>, JpaSpecificationExecutor<Ingredient> {
 }
