92f6775116338b550ca3a7f5e8aea8be08f06d43
thomas <thomas.graber310@gmail.com>
2025-01-27 22:19:23 +0100

    feat(api/user): wrong validation

--- src/main/java/graber/thomas/feastverse/controller/UserController.java
+++ src/main/java/graber/thomas/feastverse/controller/UserController.java
@@ -2,22 +2,20 @@ package graber.thomas.feastverse.controller;
 
 import graber.thomas.feastverse.dto.reports.ReportMapper;
 import graber.thomas.feastverse.dto.reports.ReportViewDTO;
-import graber.thomas.feastverse.dto.user.UpdateDto;
-import graber.thomas.feastverse.dto.user.UserMapper;
-import graber.thomas.feastverse.dto.user.UserView;
+import graber.thomas.feastverse.dto.user.*;
 import graber.thomas.feastverse.model.user.User;
 import graber.thomas.feastverse.model.user.UserType;
 import graber.thomas.feastverse.service.report.ReportService;
 import graber.thomas.feastverse.service.security.SecurityService;
 import graber.thomas.feastverse.service.user.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,20 +44,21 @@ public class UserController {
         this.reportMapper = reportMapper;
     }
 
+    @UserSwaggerDoc.UserGetOneSwaggerDoc
     @GetMapping("/{userId}")
     public UserView getUser(@PathVariable UUID userId) {
         User user = this.userService.get(userId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new EntityNotFoundException("No user found for id " + userId + "."));
 
         boolean isAdmin = securityService.hasRole(UserType.ADMINISTRATOR);
-        String currentUserId = SecurityContextHolder.getContext().getAuthentication().getName();
-        boolean requestedUserIsSelf = userId.toString().equals(currentUserId);
+        UUID currentUserId = securityService.getCurrentUserId();
+        boolean requestedUserIsSelf = userId.equals(currentUserId);
 
         if (isAdmin) {
             return userMapper.toAdminUserDto(user);
-        } else if(requestedUserIsSelf){
+        } else if (requestedUserIsSelf) {
             return userMapper.toSelfUserDto(user);
-        }else {
+        } else {
             return userMapper.toPublicUserDto(user);
         }
     }
