e1e621b6e8157512d4a71bdbeedbba8c49b8e764
thomas <thomas.graber310@gmail.com>
2025-02-01 19:19:21 +0100

    refactor(globalExceptionHandler): remove unused imports

--- src/main/java/graber/thomas/feastverse/exception/GlobalExceptionHandler.java
+++ src/main/java/graber/thomas/feastverse/exception/GlobalExceptionHandler.java
@@ -3,21 +3,17 @@ package graber.thomas.feastverse.exception;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
-import org.springframework.context.MessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.method.MethodValidationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.time.OffsetDateTime;
 import java.util.*;
 
 /**
