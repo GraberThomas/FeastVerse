d4ed92b92bf29a4e894fac9d0a5af8db07c027c2
thomas <thomas.graber310@gmail.com>
2025-02-02 11:48:39 +0100

    refactor+doc(controller/types): openAPI documentation

--- src/main/java/graber/thomas/feastverse/controller/RecipeDifficultiesController.java
+++ src/main/java/graber/thomas/feastverse/controller/RecipeDifficultiesController.java
@@ -1,20 +1,46 @@
 package graber.thomas.feastverse.controller;
 
 import graber.thomas.feastverse.model.recipes.RecipeDifficulty;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Stream;
 
 @Tag(name = "Recipes difficulty category", description = "Endpoints for recipes enumerations values")
 @RestController
 @RequestMapping("/recipes/difficulties")
 public class RecipeDifficultiesController {
+
+    @Operation(
+            summary = "Get all categories of difficulties",
+            description = "Get all categories of difficulties",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Categories retrieved successfully.",
+                            content = @io.swagger.v3.oas.annotations.media.Content(
+                                    examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                                            "[\n" +
+                                                    "  \"VERY_EASY\",\n" +
+                                                    "  \"EASY\",\n" +
+                                                    "  \"MEDIUM\",\n" +
+                                                    "  \"HARD\",\n" +
+                                                    "  \"VERY_HARD\"\n" +
+                                                    "]"
+                                    )
+                            )
+                    ),
+            }
+    )
     @GetMapping
-    public List<String> getAllQuantityTypes(){
+    public List<String> getAllDifficultiesTypes(){
         return Stream.of(RecipeDifficulty.values()).map(RecipeDifficulty::name).toList();
     }
 }
--- src/main/java/graber/thomas/feastverse/controller/RecipeQuantityController.java
+++ src/main/java/graber/thomas/feastverse/controller/RecipeQuantityController.java
@@ -2,6 +2,8 @@ package graber.thomas.feastverse.controller;
 
 import graber.thomas.feastverse.model.recipes.QuantityState;
 import graber.thomas.feastverse.model.recipes.QuantityType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,56 @@ import java.util.stream.Stream;
 @RestController
 @RequestMapping("/recipes/quantities")
 public class RecipeQuantityController {
+
+    @Operation(
+            summary = "Get all categories of quantity units",
+            description = "Get all categories of quantity units",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Quantity units retrieved successfully.",
+                            content = @io.swagger.v3.oas.annotations.media.Content(
+                                    examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                                            "[\n" +
+                                                    "  \"G\",\n" +
+                                                    "  \"MG\",\n" +
+                                                    "  \"LITER\",\n" +
+                                                    "  \"BOTTLE\",\n" +
+                                                    "  \"PINCH\"\n" +
+                                                    "  \"...\"\n" +
+                                                    "]"
+                                    )
+                            )
+                    ),
+            }
+    )
     @GetMapping("/types")
     public List<String> getAllQuantityTypes(){
         return Stream.of(QuantityType.values()).map(QuantityType::name).toList();
     }
 
+    @Operation(
+            summary = "Get all categories of quantity states",
+            description = "Get all categories of quantity states",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Quantity states retrieved successfully.",
+                            content = @io.swagger.v3.oas.annotations.media.Content(
+                                    examples = @io.swagger.v3.oas.annotations.media.ExampleObject(
+                                            "[\n" +
+                                                    "  \"WHOLE\",\n" +
+                                                    "  \"CHOPPED\",\n" +
+                                                    "  \"DICED\",\n" +
+                                                    "  \"MINCED\",\n" +
+                                                    "  \"GRATED\"\n" +
+                                                    "  \"...\"\n" +
+                                                    "]"
+                                    )
+                            )
+                    ),
+            }
+    )
     @GetMapping("/states")
     public List<String> getAllQuantitySates(){
         return Stream.of(QuantityState.values()).map(QuantityState::name).toList();
